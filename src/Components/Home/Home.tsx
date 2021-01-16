de2f92ada1d6b6cf4b72705c69635b40c917290d
Rodrigo Ramirez <rodrigo8rb@gmail.com>
2021-01-15 23:35:55 -0500

    ✨ Created Initial Home
    Created Home Component.

---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <div>
+                this is da home
+            </div>
+        </>
+    );
+}
+
+export default Home;
